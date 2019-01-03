5edb9bf0a4af761ae151c71788048132b647eede
FatimaALMUGHRABI <44682786+FatimaALMUGHRABI@users.noreply.github.com>
2019-01-03 02:08:16 +0100

    Add files via upload
    vie étudiante

---
+++ Vie étudiante/Evenements.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Accueil</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+  	<link rel="stylesheet" type="text/css" href="assets/css/knacss-unminified.css">
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+ 
+  	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  	<link rel="stylesheet" type="text/javascript" href="script.js">
+</head>
+<body>
+
+	<?php
+		require('menu_footer.php');
+	?>
+	
+	
+	<img src="photo/partnersEv.png" style='float:right; margin-top:110px; margin-right:125px; width:400px; height:500px;'>
+		<div style='margin-left:100px; margin-right:600px;'>
+			<h1 style='border-bottom: 2px solid #868e96; text-align:center; margin-bottom:25px;'>Événements</h1>
+			<p style='margin-top:25px; margin-bottom:40px;'>Les étudiants du Master MITAT se mobilisent chaque année pour organiser des évènements en lien avec leurs domaines d’étude ainsi que des visites touristiques.</p>
+			<p>Ils se rendent chaque année au grand salon international du tourisme à Madrid «La Feria International del Turismo» (FITUR) qui rassemble des milliers d’exposants et professionnels du tourisme et du transport.</p>
+			<p>Ils ont aussi la chance d’aller à l’aéroport Toulouse-Blagnac afin de rencontrer des acteurs clés dans sa gestion, et de visiter ses différents services.</p>
+			<p>Les étudiants vivant dans les régions voisines prennent aussi un réel plaisir d’organiser des journées touristiques dans leur ville ou région afin de faire découvrir ses richesses aux étudiants étrangers.</p>
+			<h4 style='border-bottom: 2px solid #868e96; margin-top:35px; margin-bottom:15px;'>Les évènements de la promotion 2017/2018 :</h4>
+			<p>7 Novembre: Conférence animée par Léa Bodossian, Secrétaire Général de Airport Regions Conference (ARC)</p>
+			<p>15 Novembre: Visite à Albi</p>
+			<p>10 Janvier: Déjeuner multiculturel organisé par les étudiants et avec l’équipe pédagogique</p>
+			<p>18 Janvier: Conférence à l’aéroport Toulouse-Blagnac animée par le Directeur des Opérations M. Alain De La Meslière, et visite de l’aéroport</p>
+			<p>19 au 22 Janvier: Séjour à Madrid pour le FITUR et découverte de la ville</p>
+		</div>
+	
+</body>
+</html>
