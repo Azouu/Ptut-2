2a35f19979f5238c99a145d804f94b685590fe7d
theoschiavi <45556777+theoschiavi@users.noreply.github.com>
2019-01-03 21:19:55 +0100

    Add files via upload

---
+++ Ptut-2-master/Taxe.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Taxes</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+  	<link rel="stylesheet" type="text/css" href="assets/css/knacss-unminified.css">
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+ 
+  	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  	<link rel="stylesheet" type="text/javascript" href="script.js">
+</head>
+<body>
+
+	<?php
+		require('menu_footer.php');
+	?>
+
+	</div>
+	
+	<div class="taxe">
+		
+			<h1> <b>Taxe d'apprentissage </b>: Investir dans les compétences de demain.</h1>
+			
+			<br />
+		
+			<div class="taxe_img grid-3 has-grid"> 
+			
+
+				<div><img src="http://www.master2mitat.univ-tlse3.fr/medias/photo/0002_1452349213460-jpg?ID_FICHE=191360"></div>
+				<div><img src="http://www.master2mitat.univ-tlse3.fr/medias/photo/0003_1452349251553-jpg?ID_FICHE=191360"></div>
+				<div><img src="http://www.master2mitat.univ-tlse3.fr/medias/photo/0004_1452349312756-jpg?ID_FICHE=191360"></div>
+
+			
+			</div>
+
+		</div>
+
+		
+</body>
+</html>
---
+++ Ptut-2-master/Vie étudiante/International.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Accueil</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+  	<link rel="stylesheet" type="text/css" href="../assets/css/knacss-unminified.css">
+  	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+ 
+  	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  	<link rel="stylesheet" type="text/javascript" href="script.js">
+</head>
+<body>
+
+	<?php
+		require('menu_footer.php');
+	?>
+	
+	
+		<img src="photo/international.jpg" style='float:right; margin-top:100px; margin-right:150px; width:350px; height:350px;'>
+		<div style='margin-left:100px; margin-right:600px;'>
+			<h1 style='text-align:center; margin-bottom:25px;'>International</h1>
+			<h4 style='border-bottom: 2px solid #868e96; margin-top:35px; margin-bottom:15px;'>Mobilité Internationale</h4>
+			<p style='margin-top:25px; margin-bottom:40px;'>Se renseigner auprès du service des relations internationales de l'université: Financements possibles.</p>
+			<h4 style='border-bottom: 2px solid #868e96; margin-top:70px; margin-bottom:15px;'>Contact pour les échanges hors Europe:</h4>
+			<h5 style='margin-bottom:10px;'>Hélène Solomiac</h5>
+			<p style='color:#868e96; display:inline;margin-bottom:10px;'>Tel: </p><p style='display:inline;'>+33 (0)5 61 55 66 24  </p>
+			</br>
+			<p style='display:inline; color:#868e96;margin-bottom:10px;'> Fax: </p><p style='display:inline;'> +33 (0)5 61 55 62 09</p>
+			</br>
+			<p style='color:#868e96; display:inline;'>Mail: </p><p style='display:inline;'>helene.solomiac@univ-tlse3.fr</p>
+		</div>
+	
+</body>
+</html>
