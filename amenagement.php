1095602cd51aa9cb788dad8510bf1f6d45c85283
theoschiavi <45556777+theoschiavi@users.noreply.github.com>
2019-01-07 18:36:19 +0100

    Add files via upload

---
+++ amenagement.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Accueil</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <link rel="stylesheet" type="text/css" href="assets/css/knacss-unminified.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" type="text/javascript" href="assets/scripts/script.js">
+    </head>
+    <body>
+
+        <?php
+            require('menu_footer.php');
+        ?>
+
+         <div class="container">
+            <div class="row txtcenter rubrique">
+                    <p class="rubrique-title"> Aménagements </p>
+             </div>
+             
+             <img src="assets/img/people-3152585_640.jpg" style='float:right; margin-top:3%; margin-right:10%; width:25%; height:25%;'>
+             
+             <div class="conteneur">
+                <h4 class="titre_paragraphe">Aménagement des études :</h4>
+                <ul>
+                    <li> <h5>Régime d'études à temps partiel</h5> </li>
+                </ul> 
+                <p>  Vous pouvez solliciter un régime d'aménagement d'études, si l'examen de votre dossier le nécessite.  </p>
+                <ul>
+                    <li> <h5>Organisation de rencontres </h5></li>
+                 </ul>
+                <p>La Mission Handicap peut provoquer des rencontres avec vos enseignants ou l'équipe pédagogique de votre formation pour évoquer les aspects spécifiques de votre scolarité et d' envisager des solutions aux problèmes rencontrés.</p>
+                 <ul>     
+                   <li><h5>Mise en place de mesures d'accompagnement</h5></li>
+                </ul>
+                 <p>Recherche de documents - prise de notes - photocopies...). </p>
+                 
+                 <h4 class="titre_paragraphe">Examens et partiels</h4>
+                 <p>La  <a href="http://www.univ-tlse3.fr/medias/fichier/examens_et_concours_1328628244834.doc?ID_FICHE=172652&INLINE=FALSE"> circulaire n° 2011-220 du 27 décembre 2011 </a>  du Ministère de l'Enseignement Supérieur et de la Recherche prévoit la possibilité d'une majoration du temps de composition, l'assistance d'un secrétaire, l'utilisation de matériel spécifique, la composition en braille etc...</p>
+                 
+                 <h4 class="titre_paragraphe">Aides complémentaires (si votre handicap le nécessite) </h4>
+                 <ul>
+                     <li>Gratuité des photocopies dans les locaux de la Mission Handicap,</li>
+                     <li>Possibilité de transcription en braille pour les non voyants (cours, TP, TD),</li>
+                     <li>Mise à disposition de codeur LPC ou intervention d'interprètes en langue des signes française pour certains cours, TP, TD, dans la limite du plafond fixé par le Ministère.</li>
+                 </ul>
+                 
+                 <h4 >Pour plus de renseignements </h4>
+                 <p>Dr Laurence Cadieux
+                     05 61 55 73 51</p>
+                 <p>Ethel Gandia
+                    05 61 55 82 13
+                    <a href="mailto:mission-handicap.referent@univ-tlse3.fr">mission-handicap.referent@univ-tlse3.fr</a> 
+                 </p>
+                 <p>Secrétariat
+                     05 61 55 78 82
+                    <a href="javascript:melA('mission-handicap.referent','','','univ-tlse3.fr');" class="mailto">mission-handicap.referent@univ-tlse3.fr</a> </p>
+             </div>
+        </div>
+    </body>
+</html>
